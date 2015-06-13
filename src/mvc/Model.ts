a272301281addb19e075cce6dde403561f0e09b4
Gevik Babakhani <gevik@blendjs.com>
2015-06-13 12:30:26 +0200

    Implemented View to Model bindings

--- src/mvc/Model.ts
+++ src/mvc/Model.ts
@@ -12,13 +12,59 @@ module Blend {
 
 			modelId:string;
 			fields:Array<string>;
+			data:any;
+			bindings:any;
 
 			constructor(config:IModelConfig) {
 				super(config);
 				if(!this.fields) {
 					this.fields = [];
 				}
-				Blend.mvc.Context.registerModel(this.modelId,this);				
+				if(!this.data) {
+					this.data = {};
+				}
+				this.bindings = {};
+				this.createFields();
+				Blend.mvc.Context.registerModel(this.modelId,this);
+			}
+
+			createFields():void {
+				var me = this;
+				me.fields.forEach(function(field){
+					var setterName = 'set'+Blend.ucFirst(field);
+					var getterName = 'get'+Blend.ucFirst(field);
+					me[setterName] = function(value:any) {
+						me.data[field] = value;
+						if(me.bindings[field]) {
+							me.bindings[field].forEach(function(setter){
+								setTimeout(function(){
+									setter.call(me,value);
+								},1);
+							});
+						}
+					}
+					me[getterName] = function() {
+						return me.data[field];
+					}
+				});
+			}
+
+			bind(field,setter:Function) {
+				if(!this.bindings[field]) {
+					this.bindings[field] = [setter];
+				} else {
+					this.bindings[field].push(setter);
+				}
+			}
+
+			setData(data:any) {
+				var me = this;
+				for(var field in data) {
+					if(me.fields.indexOf(field) !== -1) {
+						var setterName = 'set'+Blend.ucFirst(field);
+						me[setterName].call(me,[data[field]]);
+					}
+				}
 			}
 		}
 	}
--- src/mvc/View.ts
+++ src/mvc/View.ts
@@ -5,6 +5,7 @@ module Blend {
 
 		export interface IViewConfig {
 			reference?:string;
+			bindings?:any;
 			controllers?:Array<string>;
 		}
 
@@ -13,22 +14,58 @@ module Blend {
 			parent:View;
 			reference:string;
 			controllers:Array<any>;
+			bindings:any;
 
-			constructor(config?:IViewConfig,parent?:View) {
+			constructor(config?:IViewConfig) {
 				super(config);
-				if(parent) this.parent = parent;
 				this.register();
+				this.processBindings();
 			}
 
 			fireEvent(eventName:string,...args:any[]) {
 				var me = this,
 					controller;
-				this.controllers.forEach(function(ctrlId){
-					controller = Blend.mvc.Context.getController(ctrlId);
-					setTimeout(function(){
-						controller.delegate(me.reference+'.'+eventName,me,args);
-					},1);					
-				});
+				if(me.controllers) {
+					me.controllers.forEach(function(ctrlId){
+						controller = Blend.mvc.Context.getController(ctrlId);
+						setTimeout(function(){
+							controller.delegate(me.reference+'.'+eventName,me,args);
+						},1);
+					});
+				}
+			}
+
+			processBindings():void {
+				var me = this;
+				if(this.bindings) {
+					for(var key in this.bindings) {
+						if(Blend.isString(this.bindings[key])) {
+							var value:Array<string> = this.bindings[key].toString().split('.');
+							if(value.length == 2) {
+								var setterName = 'set'+Blend.ucFirst(key);
+								if(this[setterName]) {
+									var modelName = value[0];
+									var fieldName = value[1];
+									var model = Blend.mvc.Context.getModel(modelName);
+									if(model) {
+										model.bind(fieldName,function(){
+											me[setterName].apply(me,arguments);
+										});
+									} else {
+										throw Error(`Model ${modelName} does not exist!`);
+									}
+								} else {
+									throw new Error(`Unable to create binding. No setter found for ${key} (${setterName})`);
+								}
+								// check to see if we have a setter with the set"key"
+							} else {
+								throw new Error("Invalid binding key. Syntax must be [model].[field]");
+							}
+						} else {
+							throw new Error("Binding key is not a string!");
+						}
+					}
+				}
 			}
 
 			register() : void {
@@ -36,7 +73,7 @@ module Blend {
 				// an empty array is the same as having no controllers!
 				if(this.controllers && this.controllers.length == 0) {
 					this.controllers = null;
-				}				
+				}
 				if(this.reference) {
 					// has a reference but no controllers.
 					// so we travel up until we find a parent with controllers
@@ -60,7 +97,7 @@ module Blend {
 								controller.registerView(me);
 							}
 						} else {
-							throw new Error(`Controler by id ${item} was not found!`);						
+							throw new Error(`Controler by id ${item} was not found!`);
 						}
 					});
 				}
