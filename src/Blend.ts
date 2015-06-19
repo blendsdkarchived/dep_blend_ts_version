c27f19d842e187317b9fec7727388982f7b02809
Gevik Babakhani <gevik@blendjs.com>
2015-06-19 11:51:19 +0200

    Added several functions

--- src/Blend.ts
+++ src/Blend.ts
@@ -21,44 +21,109 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- module Blend {
+module Blend {
 
-	export function ucFirst(value:string) {
-		return value.charAt(0).toUpperCase() + value.slice(1);
-	}
+    export function ucFirst(value: string) {
+        return value.charAt(0).toUpperCase() + value.slice(1);
+    }
 
-	export function isArray(value:any) {
-		return Object.prototype.toString.apply(value) === '[object Array]';
-	}
+    export function isArray(value: any) {
+        return Object.prototype.toString.apply(value) === '[object Array]';
+    }
 
-	export function isObject(value:any) {
-		return (typeof value === "object" &&
-			(typeof value !== "function" &&
-			value !== null &&
-			value !== undefined &&
-			!Blend.isArray(value)));
-	}
+    export function isObject(value: any) {
+        return (typeof value === "object" &&
+            (typeof value !== "function" &&
+                value !== null &&
+                value !== undefined &&
+                !Blend.isArray(value)));
+    }
 
-	export function isNullOrUndef(value:any):boolean {
-		return (value === null || value === undefined);
-	}
+    export function isNullOrUndef(value: any): boolean {
+        return (value === null || value === undefined);
+    }
 
-	export function isString(value:any):boolean {
-		return  (typeof value === 'string');
-	}
+    export function isString(value: any): boolean {
+        return (typeof value === 'string');
+    }
 
-	export class BaseClass {
+    export function isFunction(value: any): boolean {
+        return (typeof value === 'function');
+    }
 
-		constructor(config:any) {
-			this.setConfig(config);
-		}
+    export function isInstanceOf(obj: any, clazz: any): boolean {
+        var hc = '[object HTMLCollection]';
+        if (obj.toString() === hc && clazz === 'HTMLCollection') {
+            return true;
+        } else {
+            if (Blend.isString(clazz)) {
+                var fn = new Function('', ' try { return ' + clazz + ' } catch(e) { return null };');
+                clazz = fn();
+            }
+            try {
+                var res = (obj instanceof clazz);
+                return res;
+            } catch (e) {
+                return false;
+            }
+        }
+    }
 
-		setConfig(config:any) {
-			for( var key in config ) {
-				if(config[key]) {
-					this[key] = config[key];
-				}
-			}
-		}
-	}
- }
+    export function forEach(obj: any, callback: Function, scope?: any) {
+        if (typeof HTMLCollection === 'undefined') {
+            var HTMLCollection = function() {
+                //
+            };
+        }
+        var key;
+        if (obj) {
+            if (Blend.isFunction(obj)) {
+                for (key in obj) {
+                    if (key !== 'prototype' && key !== 'length' && key !== 'name' && obj.hasOwnProperty(key)) {
+                        if (callback.call(scope, obj[key], key, obj) === false) {
+                            break;
+                        }
+                    }
+                }
+            } else if (Blend.isArray(obj)) {
+                for (key = 0; key < obj.length; key++) {
+                    if (callback.call(scope, obj[key], parseInt(key), obj) === false) {
+                        break;
+                    }
+                }
+            } else if (Blend.isInstanceOf(obj, 'HTMLCollection')) {
+                var length = obj.length, key, el;
+                for (key = 0; key !== length; key++) {
+                    el = obj.item(key);
+                    if (callback.call(scope, el, key, obj) === false) {
+                        break;
+                    }
+                }
+            } else {
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if (callback.call(scope, obj[key], key, obj) === false) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return obj;
+    }
+
+    export class BaseClass {
+
+        constructor(config: any) {
+            this.setConfig(config);
+        }
+
+        setConfig(config: any) {
+            for (var key in config) {
+                if (config[key]) {
+                    this[key] = config[key];
+                }
+            }
+        }
+    }
+}
