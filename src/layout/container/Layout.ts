67c620435e36d4f696aa43716fa2747015594632
Gevik Babakhani <gevik@blendjs.com>
2015-06-22 12:11:46 +0200

    Added basic IDictionary interface

--- src/layout/container/Layout.ts
+++ src/layout/container/Layout.ts
@@ -1,14 +1,38 @@
 /// <reference path="../Layout.ts" />
+/// <reference path="../../ui/Container.ts" />
+module Blend {
+    export module layout {
+        export module container {
 
-module Blend {	
-	export module layout {		
-		export module container {
-						
-			export class Layout extends Blend.layout.Layout {
-				
-			}
-			
-			Blend.registerClassWithAlias('layout.container',Blend.layout.container.Layout);
-		}
-	}
+            export class Layout extends Blend.layout.Layout {
+
+                view:Blend.ui.Container;
+
+                constructor(config: ILayoutConfig, view: Blend.ui.View) {
+                    var me = this;
+                    super(config,view);
+                }
+
+                performLayout() {
+                    var me = this;
+                    Blend.forEach((<Blend.ui.Container>me.view).children,function(view:Blend.ui.View){
+                        view.performLayout();
+                    });
+                    me.view.doneLayout();
+                }
+
+                render() {
+                    var me = this;
+                    var el:HTMLElement = super.render.apply(me,arguments);
+                    Blend.forEach(me.view.children,function(item:Blend.ui.View){
+                        me.view.bodyContentElement.appendChild(item.getElement());
+                    });
+                    return el;
+                }
+
+            }
+
+            Blend.registerClassWithAlias('layout.container', Blend.layout.container.Layout);
+        }
+    }
 }
