618a11da7a7afa34eede1d89de61a88ee5ee2433
Gevik Babakhani <gevik@blendjs.com>
2015-06-28 12:28:57 +0200

    Added comments and enhanced forEach

--- src/Blend.ts
+++ src/Blend.ts
@@ -3,13 +3,13 @@
 module Blend {
 
     var CSS_PREFIX = 'b-';
-    var registry:IDictionary = {};
+    var registry: IDictionary = {};
 
     /**
      * Returns enum value, either the value as number or its string representation
      */
     export function getEnumValue(objEnum: any, value: any, defaultValue?: any): any {
-        var dic:IDictionary = objEnum;
+        var dic: IDictionary = objEnum;
         if (Blend.isNumeric(value)) {
             return dic[parseInt(value)] || Blend.getEnumValue(objEnum, defaultValue);
         } else {
@@ -25,7 +25,7 @@ module Blend {
      * @param {mergeArrays} will merge arrays instead of overwriting them
      */
     export function apply(target: any, source: any, overwrite: boolean = false, mergeArrays: boolean = false): any {
-        var key:any;
+        var key: any;
         overwrite = overwrite || false;
         mergeArrays = mergeArrays || false;
         if (target && source) {
@@ -65,7 +65,7 @@ module Blend {
         if (!registry[alias]) {
             var creator = function(c: Function) {
                 return function() {
-                    var o:any = Object.create(c.prototype)
+                    var o: any = Object.create(c.prototype)
                     c.apply(o, arguments);
                     return o;
                 }
@@ -76,46 +76,71 @@ module Blend {
         }
     }
 
+    /**
+     * Get an alias from a given object. This function will look for ctype:'...'
+     * key/value in an object.
+     * @returns {string} ctype or null of nothing found.
+     */
     export function getAlias(config: any) {
         return config ? (config['alias'] || config['ctype'] || null) : null;
     }
 
-    export function createObjectWithAlias(alias: string, ...args: any[]) {
+    /**
+     * Creates an object based on an alias
+     */
+    export function createObjectWithAlias(objectConfig: IDictionary) {
+        var alias = Blend.getAlias(objectConfig);
         if (registry[alias]) {
-            return registry[alias].apply(this, args);
+            return registry[alias].apply(this, objectConfig);
         } else {
             throw new Error(`No Class with alias ${alias} is registered!`);
         }
     }
 
-    export function cssPrefix(className: string|Array<string>, resurnArray: boolean = false): string|string[] {
-        var r:Array<string> = [];
+    /**
+     * Prefixes a CSS directive with CSS_PREFIX.
+     * @returns {string/string[]}
+     */
+    export function cssPrefix(className: string|Array<string>, returnArray: boolean = false): string|string[] {
+        var r: Array<string> = [];
         if (!Blend.isArray(className)) {
             className = <Array<string>>[className];
         }
-        Blend.forEach(className, function(itm:string) {
+        Blend.forEach(className, function(itm: string) {
             r.push(CSS_PREFIX + itm);
         });
-        if (resurnArray === true) {
+        if (returnArray === true) {
             return r;
         } else {
             return r.join(' ');
         }
     };
 
+    /**
+     * Uppercases the first character in a string
+     */
     export function ucFirst(value: string) {
         return value.charAt(0).toUpperCase() + value.slice(1);
     }
 
+    /**
+     * Checks if the given value is a number
+     */
     export function isNumeric(value: any): boolean {
         // Original source: JQuery
         return value - parseFloat(value) >= 0;
     }
 
+    /**
+     * Checks if the given value is an array
+     */
     export function isArray(value: any) {
         return Object.prototype.toString.apply(value) === '[object Array]';
     }
 
+    /**
+     * Checks if the given value is an object
+     */
     export function isObject(value: any) {
         return (typeof value === "object" &&
             (typeof value !== "function" &&
@@ -124,18 +149,30 @@ module Blend {
                 !Blend.isArray(value)));
     }
 
+    /**
+     * Checks if the given value is null or undefined
+     */
     export function isNullOrUndef(value: any): boolean {
         return (value === null || value === undefined);
     }
 
+    /**
+     * Checks if the given value is a string
+     */
     export function isString(value: any): boolean {
         return (typeof value === 'string');
     }
 
+    /**
+     * Checks if the given value is a function
+     */
     export function isFunction(value: any): boolean {
         return (typeof value === 'function');
     }
 
+    /**
+     * Checks if the give value is instance of another class/function
+     */
     export function isInstanceOf(obj: any, clazz: any): boolean {
         var hc = '[object HTMLCollection]';
         if (obj.toString() === hc && clazz === 'HTMLCollection') {
@@ -154,13 +191,16 @@ module Blend {
         }
     }
 
+    /**
+     * Loops though the given object (array or dictionary) and runs a callback on each item
+     */
     export function forEach(obj: any, callback: Function, scope?: any) {
         if (typeof HTMLCollection === 'undefined') {
             var HTMLCollection = function() {
                 //
             };
         }
-        var key:any;
+        var key: any;
         if (obj) {
             if (Blend.isFunction(obj)) {
                 for (key in obj) {
@@ -177,11 +217,13 @@ module Blend {
                     }
                 }
             } else if (Blend.isInstanceOf(obj, 'HTMLCollection')) {
-                var length:number = obj.length, key:any, el:HTMLElement;
+                var length: number = obj.length, key: any, el: HTMLElement;
                 for (key = 0; key !== length; key++) {
                     el = obj.item(key);
-                    if (callback.call(scope, el, key, obj) === false) {
-                        break;
+                    if (key !== 'length') {
+                        if (callback.call(scope, el, parseInt(key), obj) === false) {
+                            break;
+                        }
                     }
                 }
             } else {
@@ -194,6 +236,5 @@ module Blend {
                 }
             }
         }
-        return obj;
     }
 }
