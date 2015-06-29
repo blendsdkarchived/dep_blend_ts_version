865582ff3a0ea01eb6c8ad44cdc0818c158916a0
Gevik Babakhani <gevik@blendjs.com>
2015-06-29 10:19:01 +0200

    Update

--- src/Common.ts
+++ src/Common.ts
@@ -4,3 +4,32 @@
 interface IDictionary {
     [name: string]: any;
 }
+
+/**
+ * Interface for describing styles
+ */
+interface IStyleConfig {
+    [name: string]: string|number;
+}
+
+/**
+ * Interface for assigning EventListeners to DOM Elements
+ */
+interface ICreateElementListeners {
+    [name: string]: EventListener
+}
+
+/**
+ * Interface for the Dom.createElement utility
+ */
+interface ICreateElement {
+    tag?: string;
+    scope?: any;
+    oid?: string;
+    cls?: string|Array<string>;
+    listeners?: ICreateElementListeners;
+    text?: string;
+    children?: Array<ICreateElement|HTMLElement>;
+    extra?: any;
+    style?: IStyleConfig;
+}
