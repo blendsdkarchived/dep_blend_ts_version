6b8075c1765d624401c78ff98411c429646d78ad
Gevik Babakhani <gevik@blendjs.com>
2015-06-29 16:34:15 +0200

    Initial implementation of the Component base class

---
+++ src/Component.ts
@@ -0,0 +1,12 @@
+/// <reference path="Common.ts" />
+/// <reference path="Blend.ts" />
+
+
+module Blend {
+    /**
+     * Base class for Components.
+     */
+    export class Component {
+
+    }
+}
