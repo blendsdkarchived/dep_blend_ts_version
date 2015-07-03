912342e250f82586d0668013925d57706eeceb03
Gevik Babakhani <gevik@blendjs.com>
2015-07-03 14:39:25 +0200

    Added layout util tests

---
+++ tests/src/tests/LayouUtil.tests.ts
@@ -0,0 +1,69 @@
+/// <reference path="../../typings/blend.d.ts" />
+/// <reference path="../TestFramework.ts" />
+/// <reference path="UIView.commons.ts" />
+
+
+TestRunner.defineTest('LayoutUtil-Fit', function(t: Blend.testing.TestRunner) {
+
+    t.isOk(Blend.LayoutUtil, 'layout util exists');
+
+    var el = Blend.Dom.createElement({
+        style: {
+            width: 200,
+            height: 200,
+            top: 0,
+            left: 0,
+            position: 'absolute',
+            'background-color': 'silver'
+        }
+    });
+
+    t.clearBody(el);
+
+    var view = new UITestView();
+    el.appendChild(view.getElement())
+    view.performLayout();
+    Blend.LayoutUtil.fitElement(view.getElement());
+
+    t.delay(function() {
+        var bounds = view.getBounds();
+        t.equal(bounds, { width: 200, height: 200, top: 0, left: 0 });
+        t.done();
+    });
+
+});
+
+
+TestRunner.defineTest('LayoutUtil-Center', function(t: Blend.testing.TestRunner) {
+
+    t.isOk(Blend.LayoutUtil, 'layout util exists');
+
+    var el = Blend.Dom.createElement({
+        style: {
+            width: 200,
+            height: 200,
+            top: 0,
+            left: 0,
+            position: 'absolute',
+            'background-color': 'silver'
+        }
+    });
+
+    t.clearBody(el);
+
+    var view = new UITestView();
+    el.appendChild(view.getElement())
+    view.performLayout();
+    view.setBounds({
+        width: 50,
+        height: 50
+    });
+    Blend.LayoutUtil.centerElement(view.getElement());
+
+    t.delay(function() {
+        var bounds = view.getBounds();
+        t.equal(bounds, { top: 100, left: 100, width: 50, height: 50 }, 'element centered');
+        t.done();
+    });
+
+});
