47eef8914b84f451e58835fee1ba8117f2904f3e
Gevik Babakhani <gevik@blendjs.com>
2015-07-07 18:57:05 +0200

    Added utility methods for removing and adding Views to a container layout.

--- src/layout/container/Layout.ts
+++ src/layout/container/Layout.ts
@@ -6,26 +6,70 @@ module Blend.layout.container {
 
     export class Layout extends Blend.layout.Layout {
 
+        protected isLayoutRendered: boolean;
+
+        private itemCssClassName: string;
+
+
         constructor(config: IContainerLayoutConfig) {
             var me = this;
             super(config);
+            me.itemCssClassName = <string>Blend.cssPrefix('layout-item');
             me.cssClassName = 'container';
+            me.isLayoutRendered = false;
         }
 
+        /**
+         * Gets the body content element of a contrainer
+         * @internal
+         */
+        getViewBodyContentElement() {
+            var me = this;
+            return <HTMLElement>me.view.getAttribute('bodyContentElement');
+        }
 
+        /**
+         * Removes the HTMLElement of a View from the ContainerView
+         */
+        removeView(view: Blend.ui.View) {
+            var me = this;
+            if (me.isViewRendered) {
+                var el: HTMLElement = view.getElement();
+                return Blend.Dom.removeElement(el);
+            }
+        }
+
+        /**
+         * Adds a View to this container Layout
+         */
+        addView(view: Blend.ui.View) {
+            var me = this;
+            if (me.isLayoutRendered) {
+                var el: HTMLElement = view.getElement()
+                view.setCssClass(me.itemCssClassName);
+                me.getViewBodyContentElement().appendChild(el);
+                me.view.invalidateLayout();
+                me.view.performLayout();
+            }
+        }
+
+        /**
+         * @override
+         */
         render() {
             var me = this,
                 containerViewEl: HTMLElement = super.render.apply(me, arguments),
-                bodyContentElement: HTMLElement = <HTMLElement>me.view.getAttribute('bodyContentElement'),
+                bodyContentElement: HTMLElement = me.getViewBodyContentElement(),
                 docFrag = document.createDocumentFragment();
 
-            Blend.forEach((<Blend.ui.ContainerView>me.view).getChildren(), function(viewItem: Blend.ui.View) {
+            Blend.forEach((<Blend.ui.ContainerView>me.view).getViews(), function(viewItem: Blend.ui.View) {
                 var el: HTMLElement = viewItem.getElement();
-                viewItem.setCssClass(<string>Blend.cssPrefix('layout-item'));
+                viewItem.setCssClass(me.itemCssClassName);
                 docFrag.appendChild(el);
             });
 
             bodyContentElement.appendChild(docFrag);
+            me.isLayoutRendered = true;
             return containerViewEl;
         }
 
