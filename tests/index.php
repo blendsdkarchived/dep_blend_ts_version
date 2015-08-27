10afc2775abf8b2f40ca0768050d6c3927e20da6
Gevik Babakhani <gevik@blendjs.com>
2015-08-27 13:01:35 +0200

    Added utility css

--- tests/index.php
+++ tests/index.php
@@ -17,6 +17,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 
+    .bg-gray {
+        background-color:#EDEDED;
+    }
+
     .log {
         font-size:14px;
     }
