90089925fa6a2d0ea3a5c137af9108e9bc4cbb3e
Gevik Babakhani <gevik@blendjs.com>
2015-09-30 03:22:11 +0200

    Removed old tests

--- tests/src/tests/mvc-context-test.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../typings/blend" />
-/// <reference path="../TestFramework" />
-/// <reference path="mvc-common" />
-
-TestRunner.defineTest('MVC', function(t: Blend.testing.TestRunner) {
-    t.isOk(Blend.mvc.Context, 'mvc context exist');
-    t.done();
-});
--- tests/src/tests/spacer.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../TestFramework" />
-TestRunner.defineTest('spacer', function(t: Blend.testing.TestRunner) {
-    var el = new Blend.ui.Spacer().getElement();
-    t.equal(el.getAttribute('class'),'b-spacer','spacer created');
-    t.done();
-
-});
