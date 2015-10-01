962e2f93c3a39ae364c60b521e88c8cdbf30df60
Gevik Babakhani <gevik@blendjs.com>
2015-10-01 23:19:02 +0200

    Updated to reflect the latest changes

--- src/Aliases.ts
+++ src/Aliases.ts
@@ -1,20 +1,11 @@
-/// <reference path="layout/Layout"/>
-/// <reference path="layout/container/Layout" />
-/// <reference path="layout/container/box/Horizontal"/>
-/// <reference path="layout/container/box/Vertical"/>
-/// <reference path="ui/container/View"/>
+/// <reference path="ui/Container"/>
+/// <reference path="layout/container/Flow" />
 /// <reference path="ui/Rectangle"/>
-/// <reference path="ui/Spacer"/>
-/// <reference path="ui/splitter/Splitter"/>
-/// <reference path="ui/panel/Panel" />
+/// <reference path="layout/container/box/Horizontal" />
+/// <reference path="layout/container/box/Vertical" />
 
-
-Blend.registerClassWithAlias('layout.default', Blend.layout.Layout);
-Blend.registerClassWithAlias('layout.container', Blend.layout.container.Layout);
+Blend.registerClassWithAlias('ui.container', Blend.ui.Container);
+Blend.registerClassWithAlias('layout.flow', Blend.layout.container.Flow);
 Blend.registerClassWithAlias('layout.hbox', Blend.layout.container.box.Horizontal);
 Blend.registerClassWithAlias('layout.vbox', Blend.layout.container.box.Vertical);
-Blend.registerClassWithAlias('ui.container', Blend.ui.container.View);
-Blend.registerClassWithAlias('ui.spacer', Blend.ui.Spacer);
-Blend.registerClassWithAlias('ui.splitter', Blend.ui.splitter.Splitter);
-Blend.registerClassWithAlias('ui.panel', Blend.ui.panel.Panel);
 Blend.registerClassWithAlias('ui.rect', Blend.ui.Rectangle);
