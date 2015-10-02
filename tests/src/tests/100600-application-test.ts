4968dd28e74bb12ac3a47cf67bddfb84fd5b0ba5
Gevik Babakhani <gevik@blendjs.com>
2015-10-02 12:25:48 +0200

    Fixed decimal testing

--- tests/src/tests/100600-application-test.ts
+++ tests/src/tests/100600-application-test.ts
@@ -14,8 +14,8 @@ TestRunner.defineTest('Application fit into window', function(t: Blend.testing.T
     t.delay(function() {
         var el: HTMLElement = app.getElement();
         var s: ViewBoundsInterface = Blend.Dom.getStyle(el, ['width', 'height']);
-        t.equal(s.width, window.innerWidth, 'full width is ok');
-        t.equal(s.height, window.innerHeight, 'full height is ok');
+        t.equal(Math.floor(<number>s.width), window.innerWidth, 'full width is ok');
+        t.equal(Math.floor(<number>s.height), window.innerHeight, 'full height is ok');
         t.done();
     });
 });
